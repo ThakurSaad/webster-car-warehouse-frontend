69d3818f4bc5ec984aadf18a49c4b6b17774df3e
thakurSaad <thakursaad613@gmail.com>
2022-05-07 21:12:46 +0600

    some routes check

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Blogs from "./Blogs/Blogs";
+import Header from "./Home/Header/Header";
 import Home from "./Home/Home/Home";
+import Register from "./Login/Register/Register";
 import NotFound from "./NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
--- src/Home/Header/Header.js
+++ src/Home/Header/Header.js
@@ -1,12 +1,6 @@
 import React from "react";
-import {
-  Button,
-  Container,
-  Form,
-  FormControl,
-  Nav,
-  Navbar,
-} from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -20,18 +14,12 @@ const Header = () => {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
             </Nav>
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
+            <Nav>
+              <Nav.Link as={Link} to="register">Register</Nav.Link>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
