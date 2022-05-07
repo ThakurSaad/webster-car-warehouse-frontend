f7209a36d92cc68f6947a2219e9e5f3e0624b3aa
thakurSaad <thakursaad613@gmail.com>
2022-05-08 02:16:34 +0600

    responsive banner

--- src/Home/Banner/Banner.js
+++ src/Home/Banner/Banner.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Banner.css";
-import banner from "../../Images/banner.jpg";
+import banner from "../../Images/banner.png";
+import banner2 from "../../Images/banner-2.png";
 
 const Banner = () => {
   return (
     <div className="position-relative">
-      <img className="img-fluid" src={banner} alt="banner" />
-      <div className="banner-desc position-absolute">
+      <img className="img-fluid d-lg-block d-none" src={banner} alt="banner" />
+      <img className="img-fluid d-block d-lg-none" src={banner2} alt="banner" />
+      <div className="banner-desc position-absolute mx-auto">
         <h1 className="banner-title">Webster Car Warehouse</h1>
         <p className="banner-text fs-3 text-white">
           We ensure best service to our customer
