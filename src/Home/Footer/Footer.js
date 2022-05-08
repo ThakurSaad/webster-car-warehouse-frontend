278ab8b65c1feeff8ae1ba9a1556d8e5d247a5bd
thakurSaad <thakursaad613@gmail.com>
2022-05-08 19:51:27 +0600

    footer done

--- src/Home/Footer/Footer.js
+++ src/Home/Footer/Footer.js
@@ -4,7 +4,9 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer bg-dark text-white">
-      <h6 className="text-center pt-5">All rights reserved &copy; Thakur Saad</h6>
+      <h6 className="text-center pt-5">
+        All rights reserved &copy; Thakur Saad
+      </h6>
     </div>
   );
 };
