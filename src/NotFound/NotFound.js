f8d789574880ff573c91e1a3ddb898ff4b226102
thakurSaad <thakursaad613@gmail.com>
2022-05-09 18:15:56 +0600

    fancy 404

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import notFound from "../Images/404.jpg";
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>404</h1>
+    <div className="text-center">
+      <img className="img-fluid" src={notFound} alt="" />
+      <Button
+        onClick={() => {
+          navigate("/");
+        }}
+        className="btn btn-lg btn-dark my-5"
+      >
+        Back To Home
+      </Button>
     </div>
   );
 };
